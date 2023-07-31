957cd27f469ed20349f27727e3c1960d1076130b
mahdi bahrami <124788322+mahdi1384er@users.noreply.github.com>
2023-07-31 21:58:43 +0330

    red

---
+++ ProjectHome/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {},
+  },
+
+  fontFamily: {
+    'sans': ['"Dela Gothic One"', 'cursive'],
+    'serif': ['Graphik', 'sans-serif'],
+  },
+  plugins: [],
+}
